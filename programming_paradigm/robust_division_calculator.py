073ccefe6eb6aba04c99f56b8d1a47e523d98ea0
Mohammad-4994 <mohammadaly494@gmail.com>
2024-10-07 00:20:41 +0300

    modified calc function

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,18 +1,19 @@
 # Adding function for safe division 
 
-def safe_divide(numerator, denominator):
+def safe_divide ():
 
     try :
         
-        float(numerator) = input("Enter numerator value: ")
-        float(denominator) = input("Enter denominator value: ")
-        result = numerator / denominator
-
+        numerator = float(input("Enter numerator value: "))
+        denominator = float(input("Enter denominator value: "))
+       
     except ZeroDivisionError : print("Error: Cannot divide by zero.")
 
     except ValueError : print("Error: Please enter numeric values only.")
 
-    else : print("The division is Safe")
+    else :
+        result = numerator / denominator 
+        print("The division is Safe")
 
     finally : 
 
