dd480064458c17ffd44812a22dfc201443d201af
Mohammad Ali <mohammadaly494@gmail.com>
2024-10-06 21:51:34 +0200

    created class floder

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,33 @@
+# Defining the class of bank account 
+
+class BankAccount: 
+
+    def __init__(self):
+
+        self.account_balance = 0 
+
+    def deposit(self):
+
+        amount = input("Enter the amount for deposit: ")
+
+        if amount >= self.account_balance :
+
+            self.account_balance = amount
+
+        else : print ("Invalid ammount , please add valid amount")
+
+    def withdraw(self) : 
+
+        amount = input("Enter the amount for withdraw: ")
+
+        if amount <= self.account_balance : 
+
+            return self.account_balance - amount
+        
+        else : return False 
+
+
+    def display_balance(self) :
+
+        return self.account_balance 
+    
