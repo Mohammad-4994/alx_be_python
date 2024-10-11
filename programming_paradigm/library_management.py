e567d90bf4ccd3278032024d3c699f3b27a7d4da
Mohammad Ali <mohammadaly494@gmail.com>
2024-10-11 23:59:10 +0200

    Added books & library classes

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,52 @@
+class Book : 
+
+    def __init__(self, title , author ) :
+        
+        self.title = title 
+        self.author = author
+        self._is_checked_out = False
+        
+    def add_book (self) :
+
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out :
+            self._is_checked_out = False
+            return True
+        return False
+    
+
+    def is_available(self):
+        return not self._is_checked_out
+
+class Library:
+    def __init__(self):
+        self._books = []  
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        
+        for book in self._books:
+            if book.title == title and book.is_available():
+                return book.check_out()
+                
+
+    def return_book(self, title):
+
+        for book in self._books:
+            if book.title == title and not book.is_available():
+               return book.return_book()
+                
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                return f"Title: {book.title}, Author: {book.author}"
+       
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,33 +5,22 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        # Create an instance of SimpleCalculator to use in tests
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        # Test basic addition
-        self.assertEqual(self.calc.add(1, 1), 2)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-        self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         # Test basic subtraction
         self.assertEqual(self.calc.subtract(2, 1), 1)
-        self.assertEqual(self.calc.subtract(1, 2), -1)
-        self.assertEqual(self.calc.subtract(0, 0), 0)
-
-    def test_multiply(self):
+        
+    def test_multiplication(self):
         # Test basic multiplication
         self.assertEqual(self.calc.multiply(3, 3), 9)
-        self.assertEqual(self.calc.multiply(-1, 5), -5)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
 
-    def test_divide(self):
+
+    def test_division(self):
         # Test basic division
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(-6, 3), -2)
-        self.assertEqual(self.calc.divide(7, 7), 1)
-
-        # Test divide by zero
-        with self.assertRaises(ZeroDivisionError):
-            self.calc.divide(10, 0)
+        
